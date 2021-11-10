9273c226db83983c1eb5213ed4c700e1e875a979
Thorsten von Eicken <tve@voneicken.com>
2021-11-10 12:08:00 -0800

    Improve USB port mapping

---
+++ udev-usb/get-hub-devices.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+
+import re
+import json
+import subprocess
+from pathlib import Path
+
+def camel_to_snake(str):
+    return re.sub('(.)([A-Z][a-z]+)', r'\1_\2', str).lower()
+
+# list all files in /dev/sensorgnome
+dev_list = [ str(p) for p in Path("/dev/sensorgnome").glob("*port=*") ]
+dev_list = ["funcubeProPlus.port=2.alsaDev=0.usbPath=1:4"]
+out = {}
+for dev in dev_list:
+    parts = dev.split(".")
+    name = parts.pop(0)
+    attr = { camel_to_snake(p[0]) : p[1] for p in [ p.split("=") for p in parts] }
+    attr["name"] = name
+    port = attr["port"]
+    out[port] = attr
+    if re.match(r'funcube.*', name):
+        out[port]["type"] = "fcd"
+        proc = subprocess.run(f"fcd -p {attr['usb_path']} -g", capture_output=True, shell=True)
+        try:
+            freq = int(proc.stdout.decode("utf-8").strip())
+            out[port]["frequency"] = f"{freq/1000000.0}Mhz"
+        except:
+            out[port]["frequency"] = "?"
+    elif re.match(r'rtlsdr.*', name):
+        out[port]["type"] = "rtlsdr"
+        out[port]["frequency"] = "?"
+    elif re.match(r'usbAudio', name):
+        out[port]["type"] = name
+        with open(f"/proc/asound/card{attr['alsa_dev']}/stream0", "r") as f:
+            for line in f:
+                m = re.search(r'(.*) at usb-musb')
+                if m:
+                    out[port]["name"] = m[0]
+    elif re.match(r'CornellTagXCVR', name):
+        out[port]["type"] = name
+        out[port]["name"] = "CTT 434MHz Rcvr"
+    elif re.match(r'disk', name):
+        out[port]["type"] = name
+    elif re.match(r'gps', name):
+        out[port]["type"] = name
+        if attr['type'] == "hat":
+            out[port]["name"] = "Adafruit GPS hat with PPS"
+        elif attr['type'] == "cape":
+            out[port]["name"] = "Compudata/Adafruit GPS cape with PPS"
+        else:
+            out[port]["name"] = "USB GPS receiver" + (" with PPS" if pps in attr else "")
+
+# add some stuff about the system
+#proc = subprocess.run("uname -a", capture_output=True, shell=True)
+
+print(json.dumps(out, indent=4))

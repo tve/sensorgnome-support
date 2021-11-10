2f5d5be71d0de69d99f82e421be441e6d94823ad
Thorsten von Eicken <tve@voneicken.com>
2021-11-10 13:17:04 -0800

    Create /dev/sensorgnome

--- sg-boot/gen-package.sh
+++ sg-boot/gen-package.sh
@@ -8,6 +8,7 @@ install -d $DEST
 install -m 755 *.sh $DEST
 install -d $DESTDIR/etc/systemd/system
 install -m 644 *.service $DESTDIR/etc/systemd/system
+install -d $DESTDIR/dev/sensorgnome
 
 # Boilerplate package generation
 cp -r DEBIAN $DESTDIR

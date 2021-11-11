225dcc50267d0ed4b9e092c7b853d4ecf244ea8d
Thorsten von Eicken <tve@voneicken.com>
2021-11-10 20:21:49 -0800

    Use raspi-control to init wifi

--- wifi-button/wifi-init.sh
+++ wifi-button/wifi-init.sh
@@ -30,48 +30,15 @@ fi
 WIFI_COUNTRY=${WIFI_COUNTRY:-US}
 
 # Configure the wifi client
-# Using: https://www.raspberrypi.com/documentation/computers/configuration.html#using-the-command-line
-sed -i -e "s/ssid=.*/ssid=\"$WIFI_SSID\"/" \
-       -e "s/psk=.*/psk=\"$WIFI_PASS\"/" \
-       -e "s/country=.*/country=$WIFI_COUNTRY/" \
-       /etc/wpa_supplicant/wpa_supplicant.conf
 rfkill unblock wlan # prob not necessary but harmless
-wpa_cli -i wlan0 reconfigure
+raspi-config nonint do_wifi_country $WIFI_COUNTRY
+raspi-config nonint do_wifi_ssid_passphrase $WIFI_SSID $WIFI_PASS
 
-# mkdir -p /etc/wpa_supplicant
-# cat <<"EOF" >/etc/wpa_supplicant/wpa_supplicant.conf
-# ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
-# update_config=1
-# country=$WIFI_COUNTRY
-# network={
-#     ssid="$WIFI_SSID"
-#     psk="$WIFI_PASS"
-# }
-# EOF
-# wpa_cli -i wlan0 reconfigure
-# 
-# # /etc/dhcpcd.conf
-# interface ap0
-# static ip_address=192.168.7.2/24
-# nohook wpa_supplicant
-# 
-# # hostapd.conf
-# country_code=US
-# interface=ap0
-# ssid=sg-hotspot
-# hw_mode=g
-# channel=1
-# macaddr_acl=0
-# auth_algs=1
-# ignore_broadcast_ssid=0
-# wpa=2
-# wpa_passphrase=sg-hotspot
-# wpa_key_mgmt=WPA-PSK
-# wpa_pairwise=TKIP
-# rsn_pairwise=CCMP
-# 
-# # /etc/dnsmasq.conf
-# interface=ap0
-# dhcp-range=192.168.7.10,192.168.7.20,255.255.255.0,24h
-# domain=local
-# address=/sg.local/192.168.7.2
+# Using: https://www.raspberrypi.com/documentation/computers/configuration.html#using-the-command-line
+#sed -i -e "s/ssid=.*/ssid=\"$WIFI_SSID\"/" \
+#       -e "s/psk=.*/psk=\"$WIFI_PASS\"/" \
+#       -e "s/country=.*/country=$WIFI_COUNTRY/" \
+#       /etc/wpa_supplicant/wpa_supplicant.conf
+#rfkill unblock wlan # prob not necessary but harmless
+#id=$(wpa_cli -i wlan0 add_network )
+#wpa_cli -i wlan0 reconfigure

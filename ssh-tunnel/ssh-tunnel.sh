2781e31075c25a53ef1627af707d84c44d36ea56
Thorsten von Eicken <tve@voneicken.com>
2021-11-16 21:08:49 -0800

    add reverse tunnel for web interface

--- ssh-tunnel/ssh-tunnel.sh
+++ ssh-tunnel/ssh-tunnel.sh
@@ -28,37 +28,37 @@ LOCAL_STREAM_PORT=59024
 
 LAST=$(date +%s)
 
-while true; do
-  if [[ -f $TUNNEL_PORT_FILE ]]; then
-      echo "Starting SSH tunnel"
-      read TUNNEL_PORT < $TUNNEL_PORT_FILE
-      set -x
-      ssh -N -T \
-          -L$LOCAL_STREAM_PORT:localhost:$REMOTE_STREAM_PORT \
-          -R$TUNNEL_PORT:localhost:22 \
-          -o ControlMaster=auto \
-          -o ControlPath=/tmp/sgremote \
-          -o ServerAliveInterval=25 \
-          -o ServerAliveCountMax=5 \
-          -i $UNIQUE_KEY_FILE \
-          -p $REMOTE_SSH_PORT \
-          $REMOTE_USER@$REMOTE_HOST
-      set +x
-      # tunnel died, sleep some before restart
-      NOW=$(date +%s)
-      ALIVE=$(($NOW-$LAST))
-      if [[ $ALIVE -gt 600 ]]; then
-        # tunnel was alive for a reasonably long time, restart almost immediately
-        echo "SSH died, was alive a long time, restarting in 10s"
-        sleep 10
-      else
-        # tunnel basically just died again, sleep somewhat longer in order not to hit the failure
-        # point so hard (and in case of cellular not to rack up charges)
-        echo "SSH died, was alive only briefly, restarting in 5min"
-        sleep 300
-      fi
-  else
-      echo "Sleeping: $TUNNEL_PORT_FILE missing"
-      sleep 60
-  fi
-done
+if [[ -f $TUNNEL_PORT_FILE ]]; then
+    echo "Starting SSH tunnel"
+    read TUNNEL_PORT < $TUNNEL_PORT_FILE
+    WEB_PORT=$(( $TUNNEL_PORT + 10000))  # yuck!
+    set -x
+    autossh -M 0 -N -T \
+        -L$LOCAL_STREAM_PORT:localhost:$REMOTE_STREAM_PORT \
+        -R$TUNNEL_PORT:localhost:22 \
+        -R$WEB_PORT:localhost:80 \
+        -o ControlMaster=auto \
+        -o ControlPath=/tmp/sgremote \
+        -o ServerAliveInterval=25 \
+        -o ServerAliveCountMax=5 \
+        -i $UNIQUE_KEY_FILE \
+        -p $REMOTE_SSH_PORT \
+        $REMOTE_USER@$REMOTE_HOST
+    set +x
+    # tunnel died, sleep some before restart
+    NOW=$(date +%s)
+    ALIVE=$(($NOW-$LAST))
+    if [[ $ALIVE -gt 600 ]]; then
+      # tunnel was alive for a reasonably long time, restart almost immediately
+      echo "SSH died, was alive a long time, restarting in 10s"
+      sleep 10
+    else
+      # tunnel basically just died again, sleep somewhat longer in order not to hit the failure
+      # point so hard (and in case of cellular not to rack up charges)
+      echo "SSH died, was alive only briefly, restarting in 5min"
+      sleep 300
+    fi
+else
+    echo "Sleeping: $TUNNEL_PORT_FILE missing"
+    sleep 60
+fi

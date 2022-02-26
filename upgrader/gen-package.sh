8f765c8fefbb987446a26891fce2be9b67a2546a
Thorsten von Eicken <tve@voneicken.com>
2022-02-26 08:51:20 -0800

    fix upgrader packaging

---
+++ upgrader/gen-package.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -e
+DESTDIR=build-temp
+rm -rf $DESTDIR
+mkdir $DESTDIR
+
+DEST=$DESTDIR/opt/sensorgnome/upgrader
+install -d $DEST
+install -m 755 *.sh $DEST
+
+# Boilerplate package generation
+cp -r DEBIAN $DESTDIR
+sed -e "/^Version/s/:.*/: $(TZ=PST8PDT date +%Y.%j)/" -i $DESTDIR/DEBIAN/control # set version: YYYY.DDD
+mkdir -p packages
+dpkg-deb --root-owner-group --build $DESTDIR ../packages
+# dpkg-deb --contents ../packages
+ls -lh ../packages

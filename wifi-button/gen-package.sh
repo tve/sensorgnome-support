6642a0dc0359858092645c1d1550007d4743444c
Thorsten von Eicken <tve@voneicken.com>
2022-06-06 15:39:10 -0700

    fix

--- wifi-button/gen-package.sh
+++ wifi-button/gen-package.sh
@@ -6,21 +6,21 @@ mkdir $DESTDIR
 make # produces dtbo
 
 # Installation moves files to where they need to go
-DEST=$(DESTDIR)/opt/sensorgnome/wifi-button
+DEST=$DESTDIR/opt/sensorgnome/wifi-button
 # install in sensorgnome dir
-install -d $(DEST)
-install -m 644 *.js $(DEST)
-install -m 755 *.sh $(DEST)
-install -m 644 gpio_pull.dtbo $(DEST)
-install -m 644 *.txt $(DEST)
-install -m 644 etc-dhcpcd.conf $(DEST)
-install -m 644 etc-hostapd.conf $(DEST)
+install -d $DEST
+install -m 644 *.js $DEST
+install -m 755 *.sh $DEST
+install -m 644 gpio_pull.dtbo $DEST
+install -m 644 *.txt $DEST
+install -m 644 etc-dhcpcd.conf $DEST
+install -m 644 etc-hostapd.conf $DEST
 # install in system location
-install -d $(DESTDIR)/etc/hostapd $(DESTDIR)/etc/systemd/system $(DESTDIR)/etc/dnsmasq.d
-install -d $(DESTDIR)/boot $(DESTDIR)/boot/overlays
-install -m 644 *.service $(DESTDIR)/etc/systemd/system
-install -m 644 etc-dnsmasq.conf $(DESTDIR)/etc/dnsmasq.d/wifi-button.conf
-#install -m 644 etc-wpa_supplicant.conf $(DESTDIR)/etc/wpa_supplicant/wpa_supplicant.conf
+install -d $DESTDIR/etc/hostapd $DESTDIR/etc/systemd/system $DESTDIR/etc/dnsmasq.d
+install -d $DESTDIR/boot $DESTDIR/boot/overlays
+install -m 644 *.service $DESTDIR/etc/systemd/system
+install -m 644 etc-dnsmasq.conf $DESTDIR/etc/dnsmasq.d/wifi-button.conf
+#install -m 644 etc-wpa_supplicant.conf $DESTDIR/etc/wpa_supplicant/wpa_supplicant.conf
 
 # Boilerplate package generation
 cp -r DEBIAN $DESTDIR

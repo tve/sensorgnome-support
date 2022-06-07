290b417be322cddb89b4b797d0e6e226954154c5
Thorsten von Eicken <tve@voneicken.com>
2022-06-07 12:39:04 -0700

    remove old unused stuff

--- unused/led_steady.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# make the 4 LED's stay on
-
-DEV=/sys/class/leds/beaglebone\:green\:usr
-DEVTRIG="${DEV}0/trigger ${DEV}1/trigger ${DEV}2/trigger ${DEV}3/trigger"
-echo default-on | tee $DEVTRIG

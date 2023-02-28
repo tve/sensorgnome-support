03e7aee91650256b9697ab4e47b411ae355cc45f
Thorsten von Eicken <tve@voneicken.com>
2023-02-28 11:29:17 -0800

    web-portal: add some debug echos

--- web-portal/init-config.sh
+++ web-portal/init-config.sh
@@ -11,9 +11,11 @@ fi
 # start the landing page and initial config app
 if [[ -f public/need_init ]]; then
     # initial config needed, ensure hotspot is on to do it
+    echo "Initial SG config needed, turning hotspot on"
     /opt/sensorgnome/wifi-button/wifi-hotspot.sh on
 else
     # initial config done, for now unconditionally turn on hot-spot, should have a setting somewhere
+    echo "Initial SG config done, turning hotspot on anyway for now..."
     /opt/sensorgnome/wifi-button/wifi-hotspot.sh on
 fi
 echo "Starting web portal/landing page app"

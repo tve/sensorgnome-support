696f410e329aabf8422a35e46c212374e8d9963f
Thorsten von Eicken <tve@voneicken.com>
2023-03-31 19:05:11 -0700

    cellular: fix postinstall

--- upgrader/do-double.sh
+++ upgrader/do-double.sh
@@ -7,6 +7,6 @@ while pgrep -x 'dpkg|apt|apt-get' > /dev/null; do sleep 1; done
 export DEBIAN_FRONTEND=noninteractive
 # make sure no one else is locking the dpkg database
 echo "Starting double update"
+touch /opt/sensorgnome/upgrader/has-double
 flock --exclusive --close /var/lib/dpkg/lock -c \
   'apt-get update; apt-get install -y -o Dpkg::Options::="--force-confold" sensorgnome'
-touch /opt/sensorgnome/upgrader/has-double

799c1f0e36fa1a5d64e0c8777231256f1b024345
Thorsten von Eicken <tve@voneicken.com>
2023-04-11 14:22:08 -0700

    upgrader: reboot after double upgrade

--- upgrader/do-double.sh
+++ upgrader/do-double.sh
@@ -9,4 +9,4 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Starting double update"
 touch /opt/sensorgnome/upgrader/has-double
 flock --exclusive --close /var/lib/dpkg/lock -c \
-  'apt-get update; apt-get install -y -o Dpkg::Options::="--force-confold" sensorgnome'
+  'apt-get update; apt-get install -y -o Dpkg::Options::="--force-confold" sensorgnome; sync; reboot'

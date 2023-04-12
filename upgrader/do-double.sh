b119f75e062151370cbc9aeab0529058092375d6
Thorsten von Eicken <tve@voneicken.com>
2023-04-12 14:13:52 -0700

    upgrader: pipe double upgrade log into log file

--- upgrader/do-double.sh
+++ upgrader/do-double.sh
@@ -2,6 +2,7 @@
 # One-time hack to perform a double upgrade when going
 # from pre 2023-080 to later versions
 # wait for install to finish
+exec >>/var/log/upgrade.log 2>&1
 echo "Waiting to perform double update"
 while pgrep -x 'dpkg|apt|apt-get' > /dev/null; do sleep 1; done
 export DEBIAN_FRONTEND=noninteractive

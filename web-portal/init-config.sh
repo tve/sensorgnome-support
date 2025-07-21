5cc8b7bfa59ac66cd6f17c7993d4f7185ba33b8f
Thorsten von Eicken <tve@voneicken.com>
2025-07-20 18:14:07 -0700

    fix generating HTTPS key/cert

--- web-portal/init-config.sh
+++ web-portal/init-config.sh
@@ -15,10 +15,11 @@ if egrep -q "^${user}:[^!x:][^:]{10,}" /etc/shadow; then
 fi
 
 # generate fresh self-signed HTTPs cert
-cd /etc/sensorgnome
-openssl req -x509 -newkey rsa:2048 -days 3650 \                                   
-  -noenc -keyout self-signed.key -out self-signed.pem -subj "/CN=sgpi.local" \   
-  -addext "subjectAltName=DNS:sgpi.local,DNS:*.sgpi.local"                       
+( cd /etc/sensorgnome
+openssl req -x509 -newkey rsa:2048 -days 3650 \
+  -noenc -keyout self-signed.key -out self-signed.pem -subj "/CN=sgpi.local" \
+  -addext "subjectAltName=DNS:sgpi.local,DNS:*.sgpi.local"
+)
 
 # start the landing page and initial config app
 if [[ -f public/need_init ]]; then
